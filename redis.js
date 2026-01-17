4ce8edc40eb97e6ecbd9338a9d8961e1cf6bf1d1
saburi004 <saburinikam@gmail.com>
2026-01-17 14:35:17 +0530

    added depolyed redis env to files

--- redis.js
+++ redis.js
@@ -1,7 +1,9 @@
 import { createClient } from "redis";
+import dotenv from "dotenv";
 
+dotenv.config();
 const redisClient = createClient({
-  url: "redis://localhost:6379"
+  url: process.env.REDIS_URL
 });
 
 redisClient.on("error", (err) => {
